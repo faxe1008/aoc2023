80a42fa8020ced2c695a39bd800a49b76846bc30
Fabian Blatz <fabianblatz@gmail.com>
2023-12-03 22:21:06 +0100

    day3, part2: solved

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,13 +4,19 @@ use std::env;
 
 const SYMBOLS : [char; 11] = ['%', '+', '#', '&', '$', '*', '\n', '=', '-', '@', '/'];
 
+const OFFSETS : [Coord2D; 8] = [
+    Coord2D {x: -1, y: -1}, Coord2D {x: 0, y: -1}, Coord2D {x: 1, y: -1},
+    Coord2D {x: -1, y: 0},  Coord2D {x: 1, y: 0},
+    Coord2D {x: -1, y: 1}, Coord2D {x: 0, y: 1}, Coord2D {x: 1, y: 1},
+];
+
 #[derive(Hash, PartialEq, Debug, Eq, Clone)]
 struct Coord2D {
     x: isize,
     y: isize
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq,)]
 struct EnginePart {
     number: u32,
     digit_coords: HashSet<Coord2D>
@@ -27,7 +33,7 @@ struct EngineSchematic {
 
 impl EngineSchematic {
 
-    fn from_string(text: &str) -> Self {
+    fn from_string(text: &str, symbols_chars: &[char]) -> Self {
         let rows : Vec<&str> = text.split('\n').collect();
 
         let width = rows[0].len();
@@ -72,7 +78,7 @@ impl EngineSchematic {
             }
     
 
-            let symbol_coords : HashSet<Coord2D> = row_text.match_indices(|c: char| SYMBOLS.contains(&c)).map(|(pos, _s)| Coord2D {x: pos as isize, y:row_index as isize}).collect();
+            let symbol_coords : HashSet<Coord2D> = row_text.match_indices(|c: char| symbols_chars.contains(&c)).map(|(pos, _s)| Coord2D {x: pos as isize, y:row_index as isize}).collect();
             for cr in symbol_coords {
                 symbol_list.insert(cr);
             }
@@ -89,18 +95,14 @@ impl EngineSchematic {
 
     fn get_valid_engine_parts_sum(&self) -> u32 {
 
-        let offsets = [
-            Coord2D {x: -1, y: -1}, Coord2D {x: 0, y: -1}, Coord2D {x: 1, y: -1},
-            Coord2D {x: -1, y: 0},  Coord2D {x: 1, y: 0},
-            Coord2D {x: -1, y: 1}, Coord2D {x: 0, y: 1}, Coord2D {x: 1, y: 1},
-        ];
+      
         let mut part_sum = 0;
 
         for engine_part in &self.parts {
 
             'loop_digits: for digit_coords in &engine_part.digit_coords {
 
-                for offs in &offsets {
+                for offs in &OFFSETS {
                     let potential_symbol_pos = Coord2D { x: digit_coords.x + offs.x, y: digit_coords.y + offs.y };
                     
                     if self.symbols.contains(&potential_symbol_pos) {
@@ -115,18 +117,53 @@ impl EngineSchematic {
         }
         part_sum
     }
+
+
+    fn get_gear_ratio(&self) -> u32 {
+        // at this point symbols only contains the gears
+
+        let mut gear_ratio = 0;
+
+        for gear in &self.symbols {
+            let mut neighbouring_engine_parts : Vec<&EnginePart> = Vec::new();
+
+            
+            for offs in &OFFSETS {
+                let potential_pos = Coord2D { x : gear.x + offs.x, y:gear.y + offs.y};
+
+                let mut neighbors : Vec<&EnginePart> = self.parts.iter().filter(|part| part.digit_coords.contains(&potential_pos)).collect();
+               
+                for neigbor in neighbors {
+                    if !neighbouring_engine_parts.contains(&neigbor) {
+                        neighbouring_engine_parts.push(neigbor);
+                    }
+                }
+            }
+           
+            if neighbouring_engine_parts.len() == 2 {
+                gear_ratio += (neighbouring_engine_parts[0].number * neighbouring_engine_parts[1].number);
+            }
+
+        }
+        gear_ratio
+    }
 }
 
 fn riddle_part_one(file_path: &String) {
     let text = fs::read_to_string(file_path).expect("Error reading file");
 
-    let schematic = EngineSchematic::from_string(&text);    
+    let schematic = EngineSchematic::from_string(&text, &SYMBOLS);    
     let part_sum = schematic.get_valid_engine_parts_sum();
     println!("{:?}", part_sum);
 }
 
 fn riddle_part_two(file_path: &String) {
-   
+    let text = fs::read_to_string(file_path).expect("Error reading file");
+
+    let gears = ['*'];
+    let schematic = EngineSchematic::from_string(&text, &gears);  
+    let gear_ratio = schematic.get_gear_ratio();
+    println!("{:?}", gear_ratio);
 }
 
 fn main() {
