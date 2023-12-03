fd1a55ae563de510ad751c4bb1a96b98cea867f0
blatzfab <fabian.blatz@hansgrohe.com>
2023-12-03 09:51:36 +0100

    day1, part2: solved

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::ops::Index;
 
 fn riddle_part_one(file_path: &String) {
     let text = fs::read_to_string(file_path).expect("Error reading file");
@@ -21,6 +22,89 @@ fn riddle_part_one(file_path: &String) {
 
 fn riddle_part_two(file_path: &String) {
    
+    let text = fs::read_to_string(file_path).expect("Error reading file");
+
+    let lines : Vec<&str> = text.split('\n').collect();
+    let mut numbers : Vec<u32> = Vec::new();
+
+    
+    let number_words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    #[derive(PartialEq)]
+    enum SearchDir {
+        Left,
+        Right
+    }
+
+    let get_first_number = |s: &str, dir: SearchDir| {
+
+        let mut first_index = match dir {
+            SearchDir::Left => s.len(),
+            SearchDir::Right => 0
+        };
+        let mut digit_value:u32 = 0;
+
+        // Search for word text
+        for (word_index, number_word) in number_words.iter().enumerate() {
+            let search_res = match dir {
+                SearchDir::Left => s.find(number_word),
+                SearchDir::Right => s.rfind(number_word)
+            };
+            if let Some(index) = search_res {
+
+                match dir {
+                    SearchDir::Left => {
+                        if index <= first_index {
+                            first_index = index;
+                            digit_value = word_index as u32 + 1;
+                        }
+                    },
+                    SearchDir::Right => {
+                        if index >= first_index {
+                            first_index = index;
+                            digit_value = word_index as u32 + 1;
+                        }
+                    }
+                };
+            }
+        }
+
+        // Search for the digits
+        let search_res = match dir {
+            SearchDir::Left => s.find(|x:char| x.is_digit(10)),
+            SearchDir::Right =>s.rfind(|x: char| x.is_digit(10))
+        };
+        if let Some(index) = search_res {
+            match dir {
+                SearchDir::Left => {
+                    if index <= first_index {
+                        first_index = index;
+                        digit_value = s.chars().nth(index).unwrap().to_digit(10).unwrap();
+                    }
+                },
+                SearchDir::Right => {
+                    if index >= first_index {
+                        first_index = index;
+                        digit_value = s.chars().nth(index).unwrap().to_digit(10).unwrap();
+                    }
+                }
+            }
+        }
+
+        digit_value
+    };
+
+    for line in lines {
+        let left = get_first_number(line, SearchDir::Left);
+        let right = get_first_number(line, SearchDir::Right);
+
+        let number = left * 10 + right;
+        println!("{:?} => {:?}", line, number);
+        numbers.push(number);
+    }
+    let sum : u32= numbers.into_iter().sum();
+    println!("{:?}", sum);
+
 }
 
 fn main() {
