73443c6bd675b0fb9373448ccb4ac417f3b43351
Fabian Blatz <fabianblatz@gmail.com>
2023-12-04 08:58:15 +0100

    day4, part2: solved

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,9 @@
 use std::cmp;
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::collections::VecDeque;
 
 #[derive(Debug)]
 struct ScratchTicket {
@@ -33,7 +35,20 @@ impl ScratchTicket {
             picks: picked_numbers,
         }
     }
-}
+
+    fn points(&self) -> u32 {
+        let matches = self.winners.intersection(&self.picks).count() as i32;
+        if matches > 0 {
+            2u32.pow(cmp::max(matches - 1, 0) as u32)
+        } else {
+            0
+        }
+    }
+
+    fn matching_numbers(&self) -> u32 {
+        self.winners.intersection(&self.picks).count() as u32
+    }
+ }
 
 fn riddle_part_one(file_path: &String) {
     let text = fs::read_to_string(file_path).expect("Error reading file");
@@ -44,27 +59,58 @@ fn riddle_part_one(file_path: &String) {
         tickets.push(ticket);
     }
 
-    let ticket_points: Vec<u32> = tickets
-        .iter()
-        .map(|ticket| {
-
-            let matches : i32 = ticket.winners.intersection(&ticket.picks).count() as i32;
-            if matches > 0 {
-                2u32.pow(cmp::max(
-                    matches - 1,
-                    0,
-                ) as u32)
-            } else {
-                0
-            }
-        })
-        .collect();
-    let point_sum:u32 = ticket_points.iter().sum();
+    let ticket_points: Vec<u32> = tickets.iter().map(|ticket| ticket.points()).collect();
+    let point_sum: u32 = ticket_points.iter().sum();
     println!("Ticket Points: {:?}", ticket_points);
     println!("Sum: {:?}", point_sum);
 }
 
-fn riddle_part_two(file_path: &String) {}
+fn riddle_part_two(file_path: &String) {
+    let text = fs::read_to_string(file_path).expect("Error reading file");
+
+    let mut tickets: Vec<ScratchTicket> = Vec::new();
+    for line in text.split('\n') {
+        let ticket = ScratchTicket::from_string(line);
+        tickets.push(ticket);
+    }
+
+
+    let mut ticket_pile: HashMap<usize, usize> = HashMap::new();
+    let mut ticket_queue : VecDeque<(usize, &ScratchTicket)> = VecDeque::new();
+
+    let ticket_count = tickets.len();
+
+    let mut incr_ticket_count = |ticket_index| {
+        ticket_pile.insert(ticket_index, ticket_pile.get(&ticket_index).unwrap_or(&0) + 1);
+    };
+
+    for (ticket_index, ticket) in tickets.iter().enumerate() {
+        let ticket_points = ticket.matching_numbers();
+        incr_ticket_count(ticket_index);
+
+        let end_index = cmp::min(ticket_index + 1 + ticket_points as usize, ticket_count);
+        for copy_index in ticket_index+1..end_index {
+            let copy_ticket = &tickets[copy_index];
+
+            ticket_queue.push_back((copy_index, copy_ticket));
+        }
+    }
+
+    while let Some((ticket_index, ticket)) = ticket_queue.pop_front() {
+        let ticket_points = ticket.matching_numbers();
+        incr_ticket_count(ticket_index);
+
+        let end_index = cmp::min(ticket_index + 1 + ticket_points as usize, ticket_count);
+        for copy_index in ticket_index+1..end_index {
+            let copy_ticket = &tickets[copy_index];
+
+            ticket_queue.push_back((copy_index, copy_ticket));
+        }
+    }
+
+    let ticket_count : usize = ticket_pile.iter().map(|(k,v)| v).sum();
+    println!("Ticket Count: {:?}", ticket_count);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
