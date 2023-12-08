711e40539ab1eeac44ae279012350b235d8918b7
Fabian Blatz <fabianblatz@gmail.com>
2023-12-08 11:08:15 +0100

    day8, part: solved

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -6,32 +6,44 @@ use std::env;
 use std::fs;
 
 
+struct WasteLandMap {
+    left_right_list:  Vec<usize>,
+    map: HashMap<String, [String; 2]>
+}
+
+impl WasteLandMap {
+    fn from_str(text: &str) -> Self {
+        let lines : Vec<&str> = text.split('\n').collect();
+
+        let left_right_list : Vec<usize> = lines[0].chars().map(|c: char| if c == 'L' { 0 } else { 1}).collect();
+
+        let mut map: HashMap<String, [String; 2]> = HashMap::new();
+
+        for line in lines.iter().skip(2){
+            let parts : Vec<&str> = line.split('=').collect();
+
+            let cur_pos = parts[0].trim();
+            let left_next = &parts[1].split(',').nth(0).unwrap().trim()[1..];
+            
+            let r_tmp = &parts[1].split(',').nth(1).unwrap().trim();
+            let right_next = &r_tmp[0..r_tmp.len()-1]; 
+    
+    
+            map.insert(cur_pos.to_string(), [left_next.to_string(), right_next.to_string()]);
+        }
+        Self {
+            left_right_list,
+            map
+        }
+    }
+}
+
+
 fn riddle_part_one(file_path: &String) {
     let text = fs::read_to_string(file_path).expect("Error reading file");
 
 
-    let lines : Vec<&str> = text.split('\n').collect();
-
-
-    let left_right_list : Vec<usize> = lines[0].chars().map(|c: char| if c == 'L' { 0 } else { 1}).collect();
-
-
-    let mut map: HashMap<&str, [&str; 2]> = HashMap::new();
-    for line in lines.iter().skip(2) {
-        // AAA = (BBB, CCC)
-
-        let parts : Vec<&str> = line.split('=').collect();
-
-        let cur_pos = parts[0].trim();
-        let left_next = &parts[1].split(',').nth(0).unwrap().trim()[1..];
-        
-        let r_tmp = &parts[1].split(',').nth(1).unwrap().trim();
-        let right_next = &r_tmp[0..r_tmp.len()-1]; 
-
-
-        map.insert(cur_pos, [left_next, right_next]);
-    }
-
+    let map = WasteLandMap::from_str(&text);
 
     let mut cur_pos = "AAA";
     let mut step_count = 0;
@@ -43,11 +55,11 @@ fn riddle_part_one(file_path: &String) {
             break;
         }
 
-        let path_index = left_right_list[cur_step_in_instructions];
-        cur_step_in_instructions = (cur_step_in_instructions + 1) % left_right_list.len();
+        let path_index = map.left_right_list[cur_step_in_instructions];
+        cur_step_in_instructions = (cur_step_in_instructions + 1) % map.left_right_list.len();
 
-        println!("Taking path: {:?}", map[cur_pos][path_index]); 
-        cur_pos = map[cur_pos][path_index];
+        println!("Taking path: {:?}", map.map[cur_pos][path_index]); 
+        cur_pos = &map.map[cur_pos][path_index];
 
         step_count += 1;
     }
@@ -55,8 +67,51 @@ fn riddle_part_one(file_path: &String) {
 
 }
 
+fn gcd(mut a: usize, mut b: usize) ->  usize{
+    while b != 0 {
+        let remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    a
+}
+
 fn riddle_part_two(file_path: &String) {
     let text = fs::read_to_string(file_path).expect("Error reading file");
+
+    let map = WasteLandMap::from_str(&text);
+
+   
+    let mut cur_positions : Vec<&String> = map.map.keys().filter(|key| key.ends_with('A')).collect();
+    println!("Found {} starting positions: {:?}", cur_positions.len(), cur_positions);
+
+
+    let mut terminations : Vec<usize> = Vec::new();
+    for i in 0..cur_positions.len() {
+
+        let mut step_count = 0;
+        let mut cur_step_in_instructions = 0;
+
+        loop {
+
+            if cur_positions[i].ends_with('Z') {
+                break;
+            }
+    
+            let path_index = map.left_right_list[cur_step_in_instructions];
+            cur_step_in_instructions = (cur_step_in_instructions + 1) % map.left_right_list.len();
+            cur_positions[i] = &map.map[cur_positions[i]][path_index];
+            step_count += 1;
+        }
+        println!("{} terminates after: {}", cur_positions[i], step_count);
+        terminations.push(step_count);
+    }
+
+    let mut lcm = 1;
+    for i in &terminations {
+        lcm = lcm * (*i as f64 / gcd(lcm, *i) as f64 ).floor() as usize;   
+    }
+    dbg!(lcm);
 
 }
 
