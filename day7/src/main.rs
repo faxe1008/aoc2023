b3c30f1892113c33a1cd26d5428f6612d55e8bb7
Fabian Blatz <fabianblatz@gmail.com>
2023-12-08 09:59:19 +0100

    day7, part2: solved

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,26 +1,26 @@
 use core::panic;
 use std::cmp::Ordering;
+use std::cmp::PartialOrd;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::cmp::PartialOrd;
 use std::hash::RandomState;
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Hash, Clone, Copy)]
 enum Card {
-    Ace=12,
-    King=11,
-    Queen=10,
-    Jet=9,
-    Ten=8,
+    Ace = 12,
+    King = 11,
+    Queen = 10,
+    Ten = 8,
     Nine = 7,
     Eight = 6,
     Seven = 5,
     Six = 4,
-    Five= 3,
-    Four= 2,
-    Three =1,
-    Two =0
+    Five = 3,
+    Four = 2,
+    Three = 1,
+    Two = 0,
+    Jet = -1,
 }
 
 impl From<char> for Card {
@@ -39,7 +39,7 @@ impl From<char> for Card {
             '4' => Card::Four,
             '3' => Card::Three,
             '2' => Card::Two,
-            _ => panic!("Unknown Card")
+            _ => panic!("Unknown Card"),
         }
     }
 }
@@ -48,59 +48,98 @@ impl From<char> for Card {
 struct CardHand {
     cards: HashMap<Card, usize>,
     raw_txt: String,
-    bid: usize
+    bid: usize,
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
 enum CardHandType {
     FiveOfKind = 6,
-    FourOfKind= 5,
+    FourOfKind = 5,
     FullHouse = 4,
     ThreeOfKind = 3,
     TwoPair = 2,
     OnePair = 1,
-    HighCard = 0
+    HighCard = 0,
+}
+
+impl From<&str> for CardHand {
+    fn from(value: &str) -> Self {
+        CardHand {
+            cards: value
+                .split(' ')
+                .nth(0)
+                .unwrap()
+                .chars()
+                .fold(HashMap::new(), |mut map, c| {
+                    *map.entry(Card::from(c)).or_insert(0) += 1;
+                    map
+                }),
+            raw_txt: value.split(' ').nth(0).unwrap().to_string(),
+            bid: value.split(' ').nth(1).unwrap().parse::<usize>().unwrap(),
+        }
+    }
 }
 
 impl CardHand {
-
     fn find_by_count(&self, count: usize) -> Option<Card> {
-        self.cards.iter().find_map(|(&card, &c)| 
-            if c == count {
+        let joker_count = *self.cards.get(&Card::Jet).unwrap_or(&0);
+
+        self.cards.iter().find_map(|(&card, &c)| {
+            let occs = if card == Card::Jet {
+                c - joker_count
+            } else {
+                c
+            };
+            if occs + joker_count == count {
                 Some(card)
-            }else {
+            } else {
                 None
             }
-        )
+        })
     }
 
     fn get_type(&self) -> CardHandType {
-       
-       if self.find_by_count(5).is_some() {
-        return CardHandType::FiveOfKind;
-       } else if self.find_by_count(4).is_some() {
-         return CardHandType::FourOfKind;
-       }
+        let joker_count = *self.cards.get(&Card::Jet).unwrap_or(&0);
 
-       if self.find_by_count(3).is_some(){
-            if self.cards.len() == 2 {
+        if self.find_by_count(5).is_some() {
+            return CardHandType::FiveOfKind;
+        } else if self.find_by_count(4).is_some() {
+            return CardHandType::FourOfKind;
+        }
+
+        if self.find_by_count(3).is_some() {
+            if self.cards.len() == 2 || (self.cards.len() == 3 && joker_count == 1) {
                 return CardHandType::FullHouse;
             } else if self.cards.len() == 3 {
                 return CardHandType::ThreeOfKind;
             } else {
-                panic!("WTF");
+                return CardHandType::ThreeOfKind;
             }
-       }
+        }
 
-       if let Some(card) = self.find_by_count(2){
-            if self.cards.iter().find_map(|(cd, &count)| if count == 2 && *cd != card { Some(cd)} else { None }).is_some() {
+        if let Some(card) = self.find_by_count(2) {
+
+            let second_pair_card = self
+            .cards
+            .iter()
+            .find_map(|(cd, &count)| {
+                if count == 2 + joker_count && *cd != card {
+                    Some(cd)
+                } else {
+                    None
+                }
+            });
+
+            // 644J1
+            if second_pair_card.is_some() && self.cards.len() == 3
+            {
                 return CardHandType::TwoPair;
-            }else {
+            } else {
                 return CardHandType::OnePair;
             }
-       }
+        }
 
-       CardHandType::HighCard
+        CardHandType::HighCard
     }
 }
 
@@ -118,23 +157,20 @@ impl PartialOrd for CardHand {
         }
 
         for (self_char, other_char) in self.raw_txt.chars().zip(other.raw_txt.chars()) {
-                
-                let self_card = Card::from(self_char);
-                let other_card = Card::from(other_char);
+            let self_card = Card::from(self_char);
+            let other_card = Card::from(other_char);
 
-                match self_card.cmp(&other_card) {
-                    Ordering::Equal => continue,
-                    e => return Some(e),
-                };
+            match self_card.cmp(&other_card) {
+                Ordering::Equal => continue,
+                e => return Some(e),
+            };
         }
 
         return None;
     }
 }
 
-impl Eq for CardHand {
-
-}
+impl Eq for CardHand {}
 
 impl Ord for CardHand {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -144,14 +180,13 @@ impl Ord for CardHand {
         }
 
         for (self_char, other_char) in self.raw_txt.chars().zip(other.raw_txt.chars()) {
-                
-                let self_card = Card::from(self_char);
-                let other_card = Card::from(other_char);
+            let self_card = Card::from(self_char);
+            let other_card = Card::from(other_char);
 
-                match self_card.cmp(&other_card) {
-                    Ordering::Equal => continue,
-                    e => return e,
-                };
+            match self_card.cmp(&other_card) {
+                Ordering::Equal => continue,
+                e => return e,
+            };
         }
 
         return Ordering::Equal;
@@ -161,36 +196,24 @@ impl Ord for CardHand {
 fn riddle_part_one(file_path: &String) {
     let text = fs::read_to_string(file_path).expect("Error reading file");
 
-    let lines : Vec<&str> = text.split('\n').collect();
+    let lines: Vec<&str> = text.split('\n').collect();
 
-    let mut hands : Vec<CardHand> = lines.iter().filter(|l| !l.is_empty()).map(|line| 
-        
-        CardHand {
-            cards: line.split(' ').nth(0).unwrap().chars()
-            .fold(HashMap::new(), |mut map, c| {
-                *map.entry(Card::from(c)).or_insert(0) += 1;
-                map
-            }),
-            raw_txt: line.split(' ').nth(0).unwrap().to_string(),
-            bid: line.split(' ').nth(1).unwrap().parse::<usize>().unwrap()
-        }
-    ).collect();
+    let mut hands: Vec<CardHand> = lines
+        .iter()
+        .filter(|l| !l.is_empty())
+        .map(|line| CardHand::from(*line))
+        .collect();
 
-    
     hands.sort();
-   
+
     let mut winnings = 0;
     for (rank, hand) in hands.iter().enumerate() {
         winnings += hand.bid * (rank + 1);
     }
     println!("{:?}", winnings);
-
-    
-
 }
 
-fn riddle_part_two(file_path: &String) {
-}
+fn riddle_part_two(file_path: &String) {}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -215,3 +238,58 @@ fn main() {
         }
     };
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{CardHandType, CardHand};
+
+    #[test]
+    fn check_jet_cases() {
+        let samples: Vec<(&'static str, CardHandType)> = vec![
+            ("5J984 114", CardHandType::OnePair),
+            ("787J8 587", CardHandType::FullHouse),
+            ("7JJ77 757", CardHandType::FiveOfKind),
+            ("A7T4J 166", CardHandType::OnePair),
+            ("Q9A7J 843", CardHandType::OnePair),
+            ("8J544 827", CardHandType::ThreeOfKind),
+            ("9JQ44 950", CardHandType::ThreeOfKind),
+            ("963J5 490", CardHandType::OnePair),
+            ("7JJ99 91", CardHandType::FourOfKind),
+            ("Q6QQJ 571", CardHandType::FourOfKind),
+            ("68J8T 395", CardHandType::ThreeOfKind),
+            ("5JJK9 285", CardHandType::ThreeOfKind),
+            ("A94AJ 636", CardHandType::ThreeOfKind),
+            ("777J8 445", CardHandType::FourOfKind),
+            ("JTK7J 7", CardHandType::ThreeOfKind),
+            ("96J29 249", CardHandType::ThreeOfKind),
+            ("7J44Q 603", CardHandType::ThreeOfKind),
+            ("T6JT6 538", CardHandType::FullHouse),
+            ("T9AAJ 299", CardHandType::ThreeOfKind),
+            ("J5559 357", CardHandType::FourOfKind),
+            ("T7AJ7 569", CardHandType::ThreeOfKind),
+            ("J7TTK 155", CardHandType::ThreeOfKind),
+            ("6QJJQ 500", CardHandType::FourOfKind),
+            ("5JJ5J 864", CardHandType::FiveOfKind),
+            ("6QJJ6 158", CardHandType::FourOfKind),
+            ("T7J77 939", CardHandType::FourOfKind),
+            ("7J5T7 337", CardHandType::ThreeOfKind),
+            ("JQ478 431", CardHandType::OnePair),
+            ("QQ9QJ 977", CardHandType::FourOfKind),
+            ("TAJQK 710", CardHandType::OnePair),
+            ("J4422 678", CardHandType::FullHouse),
+            ("J8JJ6 290", CardHandType::FourOfKind),
+            ("644J6 659",CardHandType::FullHouse),
+            ("J3333 928", CardHandType::FiveOfKind),
+            ("J998A 349",  CardHandType::ThreeOfKind),
+        ];
+
+        for (txt, cht) in &samples {
+            let c = CardHand::from(*txt);
+            assert_eq!(c.get_type(), *cht, "{} should be {:?}", c.raw_txt, *cht);
+        }
+
+    }
+}
+
+
